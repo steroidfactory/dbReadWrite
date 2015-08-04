ef7f84915620a087861f3fc156efe14fb40cc4f2
steroidfactory@gmail.com <steroidfactory@gmail.com>
2015-08-04 13:44:08 -0500

    Working

--- dbReadWrite/App/Form1.Designer.cs
+++ dbReadWrite/App/Form1.Designer.cs
@@ -29,109 +29,203 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.input3 = new System.Windows.Forms.TextBox();
             this.input2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.input1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.listQT = new System.Windows.Forms.ComboBox();
+            this.readBox = new System.Windows.Forms.DataGridView();
+            this.readQT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.readOrderNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.readID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.readTrackingNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.readBox)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 163F));
-            this.tableLayoutPanel1.Controls.Add(this.input3, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.input2, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.input1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.651944F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.24832F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.24832F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.85142F));
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.input3, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.input2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.input1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listQT, 0, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(69, 28);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(501, 100);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(501, 85);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // input3
+            // label4
             // 
-            this.input3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.input3.Location = new System.Drawing.Point(341, 53);
-            this.input3.Name = "input3";
-            this.input3.Size = new System.Drawing.Size(157, 20);
-            this.input3.TabIndex = 3;
-            this.input3.TextChanged += new System.EventHandler(this.input3_TextChanged);
-            // 
-            // input2
-            // 
-            this.input2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.input2.Location = new System.Drawing.Point(172, 53);
-            this.input2.Name = "input2";
-            this.input2.Size = new System.Drawing.Size(163, 20);
-            this.input2.TabIndex = 2;
-            this.input2.TextChanged += new System.EventHandler(this.input2_TextChanged);
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 48);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "QT";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(341, 0);
+            this.label3.Location = new System.Drawing.Point(343, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(157, 50);
+            this.label3.Size = new System.Drawing.Size(155, 48);
             this.label3.TabIndex = 0;
             this.label3.Text = "Tracking Number";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 50);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Order Number";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // input1
-            // 
-            this.input1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.input1.Location = new System.Drawing.Point(3, 53);
-            this.input1.Name = "input1";
-            this.input1.Size = new System.Drawing.Size(163, 20);
-            this.input1.TabIndex = 1;
-            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(172, 0);
+            this.label2.Location = new System.Drawing.Point(197, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(163, 50);
+            this.label2.Size = new System.Drawing.Size(140, 48);
             this.label2.TabIndex = 0;
             this.label2.Text = "Employee ID";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Order Number";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // input3
+            // 
+            this.input3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.input3.Location = new System.Drawing.Point(343, 51);
+            this.input3.Name = "input3";
+            this.input3.Size = new System.Drawing.Size(155, 20);
+            this.input3.TabIndex = 3;
+            this.input3.TextChanged += new System.EventHandler(this.input3_TextChanged);
+            this.input3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.input3_KeyDown);
+            // 
+            // input2
+            // 
+            this.input2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.input2.Location = new System.Drawing.Point(197, 51);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(140, 20);
+            this.input2.TabIndex = 2;
+            this.input2.TextChanged += new System.EventHandler(this.input2_TextChanged);
+            this.input2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.input2_KeyDown);
+            // 
+            // input1
+            // 
+            this.input1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.input1.Location = new System.Drawing.Point(51, 51);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(140, 20);
+            this.input1.TabIndex = 1;
+            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
+            this.input1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.input1_KeyDown);
+            // 
+            // listQT
+            // 
+            this.listQT.FormattingEnabled = true;
+            this.listQT.Location = new System.Drawing.Point(3, 51);
+            this.listQT.Name = "listQT";
+            this.listQT.Size = new System.Drawing.Size(42, 21);
+            this.listQT.TabIndex = 4;
+            this.listQT.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.listQT.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listQT_KeyDown);
+            // 
+            // readBox
+            // 
+            this.readBox.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.readBox.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.readQT,
+            this.readOrderNumber,
+            this.readID,
+            this.readTrackingNumber});
+            this.readBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.readBox.Location = new System.Drawing.Point(0, 0);
+            this.readBox.MultiSelect = false;
+            this.readBox.Name = "readBox";
+            this.readBox.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.readBox.Size = new System.Drawing.Size(501, 179);
+            this.readBox.TabIndex = 11;
+            this.readBox.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.readBox_CellMouseClick);
+            // 
+            // readQT
+            // 
+            this.readQT.HeaderText = "QT";
+            this.readQT.Name = "readQT";
+            this.readQT.ReadOnly = true;
+            this.readQT.Width = 25;
+            // 
+            // readOrderNumber
+            // 
+            this.readOrderNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.readOrderNumber.HeaderText = "Order Number";
+            this.readOrderNumber.Name = "readOrderNumber";
+            this.readOrderNumber.ReadOnly = true;
+            // 
+            // readID
+            // 
+            this.readID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.readID.HeaderText = "Employee ID";
+            this.readID.Name = "readID";
+            this.readID.ReadOnly = true;
+            // 
+            // readTrackingNumber
+            // 
+            this.readTrackingNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.readTrackingNumber.HeaderText = "Tracking Number";
+            this.readTrackingNumber.Name = "readTrackingNumber";
+            this.readTrackingNumber.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.readBox);
+            this.panel1.Location = new System.Drawing.Point(69, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(501, 179);
+            this.panel1.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(689, 321);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.readBox)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -139,12 +233,20 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TextBox input1;
         private System.Windows.Forms.TextBox input3;
-        private System.Windows.Forms.TextBox input2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox listQT;
+        private System.Windows.Forms.DataGridView readBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn readQT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn readOrderNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn readID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn readTrackingNumber;
     }
 }
 
--- dbReadWrite/App/Form1.cs
+++ dbReadWrite/App/Form1.cs
@@ -15,7 +15,11 @@ namespace App
         public Form1()
         {
             InitializeComponent();
+            addQ();
+            input1.Focus();
         }
+
+        //public Dictionary<int, List<string>> portsText = new Dictionary<int, List<string>>();
 
         private void input1_TextChanged(object sender, EventArgs e)
         {
@@ -29,7 +33,84 @@ namespace App
 
         private void input3_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
+
+        private void input1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                input2.Focus();
+            }
+        }
+
+        private void input2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                input3.Focus();
+            }
+        }
+
+        private void input3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                addReadBox(listQT.SelectedIndex + 1, input1.Text, input2.Text, input3.Text);
+                resetInput();
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void addQ()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                Task taskA = Task.Factory.StartNew(() => listQT.Items.Add(i));
+                taskA.Wait();
+            }
+            listQT.SelectedIndex = 0;
+        }
+
+        private void listQT_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                input1.Focus();
+            }
+        }
+
+
+        private void resetInput()
+        {
+            listQT.SelectedIndex = 0;
+            input1.Clear();
+            input2.Clear();
+            input3.Clear();
+            input1.Focus();
+        }
+        private void addReadBox (int QT, string orderNumber, string ID, string trackingNumber)
+        {
+            string[] data = { QT.ToString(), orderNumber.ToString(), ID.ToString(), trackingNumber.ToString() };
+            readBox.Rows.Add(data);
+        }
+
+        private void readBox_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Console.WriteLine(readBox.Rows[0].Cells[0].Value.ToString());
+            input1.Text = readBox.Rows[readBox.SelectedRows.ToString()]
+            //Console.WriteLine(readBox.CurrentCell.RowIndex.ToString);
+        }
+
+      
     }
 }
